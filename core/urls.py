16a58a051d605277b12b2afe56cece42584e8bef
isasiluispy <isasiluispy@gmail.com>
2019-08-15 22:08:55 -0400

    django rest support

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns = [
+
+    urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+        path('api-auth/', include('rest_framework.urls')),
+    ]
