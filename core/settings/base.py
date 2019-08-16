8f7a0a23fcb9821a5653a857a0d72c9bad9a453e
isasiluispy <isasiluispy@gmail.com>
2019-08-15 22:14:32 -0400

    app for User logic

--- core/settings/base.py
+++ core/settings/base.py
@@ -72,3 +72,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# default user model
+AUTH_USER_MODEL = 'auth.User'
